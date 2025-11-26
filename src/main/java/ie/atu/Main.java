86cce39a5656debd4d5a4ee136c343cd6e06b712
OLIWIERKARDYNAL-STUD <G00434405@atu.ie>
2025-11-26 15:54:35 +0000

    Added person.java and used toString() override to display default name address and phone number

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -2,8 +2,16 @@ package ie.atu;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
+public class Main
+{
+    public static void main(String[] args)
+    {
+    Person firstPerson = new Person();
+    firstPerson.setName("Oliwier");
+    firstPerson.setAddress("Gort");
+    firstPerson.setPhoneNumber("10292210");
+
+    System.out.println(firstPerson.toString());
 
     }
 }
